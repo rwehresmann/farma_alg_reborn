11021985af0feb314bb584823fcff8a8ec0d02b8
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-12-30 19:26:25 +0000

    Add gravatar image to header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
 
   # Returns the Gravatar for the given user.
-  def gravatar_for(user, size = 80)
+  def gravatar_for(user, options = { size: 80, class: "img-circle" })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: "img-circle")
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{options[:size]}"
+    image_tag(gravatar_url, alt: user.name, class: options[:class])
   end
 end
