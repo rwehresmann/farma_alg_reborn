600f73192254d625093bc45a108f12e24e685ca3
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-12-31 18:22:24 +0000

    Add pages to creatios and displaying of the lerarning objects

--- app/controllers/learning_objects_controller.rb
+++ app/controllers/learning_objects_controller.rb
@@ -1,11 +1,22 @@
 class LearningObjectsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @learning_objects = LearningObject.where(user: current_user)
   end
 
   def new
+    @learning_object = LearningObject.new
   end
 
   def create
+    @learning_object = current_user.learning_objects.build(learning_object_params)
+    if @learning_object.save
+      flash[:success] = "Objeto de Aprendizagem criado!"
+      redirect_to @learning_object
+    else
+      render "new"
+    end
   end
 
   def edit
@@ -19,4 +30,10 @@ class LearningObjectsController < ApplicationController
 
   def show
   end
+
+    private
+
+    def learning_object_params
+      params.require(:learning_object).permit(:title, :description, :available)
+    end
 end
