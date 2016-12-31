64f54165dcafccfadcd609c6edf873b237f1e1ba
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-12-31 13:22:12 +0000

    Generate learning objects controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :learning_objects
+
   devise_for :users
   get 'dashboard/home'
   root to: 'dashboard#home'
---
+++ spec/views/learning_objects/show.html.erb_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "learning_objects/show.html.erb", type: :view do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
