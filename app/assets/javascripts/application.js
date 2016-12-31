a08c9d258601b4a6ae841a1b305133094916c559
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-12-31 00:48:11 +0000

    Remove turbolinks from manifest (it doesn't work well with the template)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+// require turbolinks
 //= require AdminLTE/bootstrap/js/bootstrap
 //= require AdminLTE/dist/js/app
 //= require_tree .
