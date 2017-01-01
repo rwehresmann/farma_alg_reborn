cadc6fe15ff910b0a3ab59a0a7af257a804a0e42
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-01-01 18:39:09 +0000

    Add method to deal with markdown text

--- app/helpers/exercises_helper.rb
+++ app/helpers/exercises_helper.rb
@@ -1,4 +1,25 @@
 module ExercisesHelper
+  # 'CodeRayify' class implementation taken from
+  # http://allfuzzy.tumblr.com/post/27314404412/markdown-and-code-syntax-highlighting-in-ruby-on
+  class CodeRayify < Redcarpet::Render::HTML
+    def block_code(code, language)
+      CodeRay.scan(code, language).div(:line_numbers => :table)
+    end
+  end
+
+  # Display in markdown ("prettified").
+  def markdown(text)
+    coderayified = CodeRayify.new(no_images: true)
+
+    extensions = { hard_wrap: true, filter_html: true, autolink: true,
+                   no_intraemphasis: true, fenced_code_blocks: true,
+                   tables: true, superscript: true }
+
+    markdown = Redcarpet::Markdown.new(coderayified, extensions)
+    markdown.render(text).html_safe
+  end
+
+  # Pluralize question text.
   def pluralize_questions(count)
     return "#{count} questões cadastradas" if count >= 0
     "#{count} questão cadastrada"
