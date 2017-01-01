a411b7a5fa6c62493090ae44ca0a2dd191979198
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-01-01 23:49:21 +0000

    Implement standard action to exercise controller and its respective views

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,12 +1,59 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_exercise
+  before_action :find_question, only: [:show, :edit, :update, :destroy]
+
   def index
-    @exercise = Exercise.find(params[:exercise_id])
     @questions = Question.where(exercise: @exercise)
   end
 
   def new
+    @question = Question.new
   end
 
   def create
+    @question = @exercise.questions.build(question_params)
+    if @question.save
+      flash[:success] = "Questão criada e adicionada ao exercício!"
+      redirect_to [@exercise, @question]
+    else
+      render 'new'
+    end
   end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @question.update_attributes(question_params)
+      flash[:success] = "Questão atualizada!"
+      redirect_to [@exercise, @question]
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @question.destroy
+    flash[:success] = "Questão excluída!"
+    redirect_to exercise_questions_url(@exercise)
+  end
+
+    private
+
+    def question_params
+      params.require(:question).permit(:description, :exercise_id)
+    end
+
+    def find_exercise
+      @exercise = Exercise.find(params[:exercise_id])
+    end
+
+    def find_question
+      @question = Question.find(params[:id])
+    end
 end
