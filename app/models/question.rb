4e9b5ff6718abe8f36ec018f6cf238cf9ea90a33
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-01-02 14:30:34 +0000

    Add references in the relationship between TCs and questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,5 @@ class Question < ApplicationRecord
   validates_presence_of :description
 
   belongs_to :exercise
+  has_many :test_cases
 end
--- app/models/test_case.rb
+++ app/models/test_case.rb
@@ -1,2 +1,5 @@
 class TestCase < ApplicationRecord
+  validates_presence_of :input, :output
+
+  belongs_to :question
 end
