0f5532265da69a0ed00f166b262fddf3990f52c9
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-01-02 21:55:07 +0000

    Implement index action to test case controller and add its view

--- app/controllers/test_cases_controller.rb
+++ app/controllers/test_cases_controller.rb
@@ -1,7 +1,10 @@
 class TestCasesController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_test_case, only: [:show, :edit, :update, :destroy]
+  before_action :find_question, only: [:index, :new, :create]
 
   def index
+    @test_cases = TestCase.where(question: @question)
   end
 
   def new
@@ -21,4 +24,18 @@ class TestCasesController < ApplicationController
 
   def destroy
   end
+
+    private
+
+    def test_case_params
+      params.require(:test_case).permit(:description, :input, :output, :question_id)
+    end
+
+    def find_test_case
+      @test_case = TestCase.find(params[:id])
+    end
+
+    def find_question
+      @question = Question.find(params[:question_id])
+    end
 end
