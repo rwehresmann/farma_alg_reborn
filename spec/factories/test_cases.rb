bdb2cb74a4ac6ae9d81ab36b74adc411d2af1f0e
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-01-02 14:39:27 +0000

    Add test case model tests

--- spec/factories/test_cases.rb
+++ spec/factories/test_cases.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :test_case do
-    
+    description "test case description"
+    input "input"
+    output "output"
+    question
   end
 end
--- spec/models/test_case_spec.rb
+++ spec/models/test_case_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe TestCase, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations -->" do
+    let(:test_case) { build(:test_case) }
+
+    it "is valid with valid attributes" do
+      expect(test_case).to be_valid
+    end
+
+    it "is invalid with empty input" do
+      test_case.input = ""
+      expect(test_case).to_not be_valid
+    end
+
+    it "is invalid with empty output" do
+      test_case.output = ""
+      expect(test_case).to_not be_valid
+    end
+  end
+
+  describe "Relationships -->" do
+    it "belongs to question" do
+      expect(relationship_type(TestCase, :question)).to eq(:belongs_to)
+    end
+  end
 end
