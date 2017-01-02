7ed7831bd046311e8d08386b5dbf7bade4c8b3f1
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-01-02 21:32:54 +0000

    Add test cases link to question box view

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,7 @@
 module QuestionsHelper
+  # Pluralize test case text.
+  def pluralize_test_cases(count)
+    return "#{count} casos de testes cadastrados" if count >= 0
+    "#{count} caso de teste cadastrado"
+  end
 end
