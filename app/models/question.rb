fa7a91c58f51cf46f019d89af0b7cce4c668c55e
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-01-06 01:22:31 +0000

    Implement test_all method

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,25 @@
+require 'utils/compilers'
+
 class Question < ApplicationRecord
+  include Compilers
+
   validates_presence_of :description
 
   belongs_to :exercise
   has_many :test_cases
+
+  # Run the source code with the specified input of each test case and check
+  # its output.
+  def test_all(file_name, file_ext, source_code)
+    results = []
+    self.test_cases.each do |test_case|
+      output = run(file_name, file_ext, source_code, test_case.input)
+      if has_error? || output != test_case.output
+        results << { title: test_case.title, status: :error, output: output }
+      else
+        results << { title: test_case.title, status: :success, output: output }
+      end
+    end
+    results
+  end
 end
