66b991a6512cb7e6d30cae990491fa052cf1ff21
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-01-06 02:25:51 +0000

    Fix file_name bug

--- app/models/question.rb
+++ app/models/question.rb
@@ -12,8 +12,10 @@ class Question < ApplicationRecord
   # its output.
   def test_all(file_name, file_ext, source_code)
     results = []
-    self.test_cases.each do |test_case|
-      output = run(file_name, file_ext, source_code, test_case.input)
+    self.test_cases.each_with_index do |test_case, index|
+      # 'file_name' must be composed by the index to avoid that 'run' method
+      # doesn't write the compiler output in the same file.
+      output = run("#{file_name}_#{index}", file_ext, source_code, test_case.input)
       if has_error? || output != test_case.output
         results << { title: test_case.title, status: :error, output: output }
       else
