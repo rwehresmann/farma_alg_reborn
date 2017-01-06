d77be295bb6a00075bbee5c2674de437574ffe6f
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-01-06 22:36:17 +0000

    Add question crumbs

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -18,3 +18,25 @@ crumb :edit_exercise do |exercise|
   link "Editar", edit_exercise_path(exercise)
   parent :exercise, exercise
 end
+
+## Question crumbs ##
+
+crumb :questions do |exercise|
+  link "Questões", exercise_questions_path(exercise)
+  parent :exercise, exercise
+end
+
+crumb :question do |question|
+  link "Questão", question
+  parent :questions, question.exercise
+end
+
+crumb :new_question do |exercise|
+  link "Novo", new_exercise_question_path
+  parent :questions, exercise
+end
+
+crumb :edit_question do |question|
+  link "Editar", edit_question_path(question)
+  parent :question, question
+end
