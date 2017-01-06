0d21f184b4f6cb64d3292f00ac6f6bb38e7fbd13
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-01-06 16:33:02 +0000

    Add test to 'create question' feature

---
+++ spec/features/create_a_test_case_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe "Create a test case", type: :feature do
+  let(:test_case) { build(:test_case) }
+  before do
+    login_as create(:user)
+    question = create(:question)
+    visit new_question_test_case_url(question.id)
+  end
+
+  context "whit valid attributes" do
+    subject do
+      fill_in "test_case_title", with: test_case.title
+      fill_in "test_case_description", with: test_case.description
+      fill_in "test_case_input", with: test_case.input
+      fill_in "test_case_output", with: test_case.output
+      click_on "Criar"
+    end
+
+    it "create the test case" do
+      expect{ subject }.to change(TestCase, :count).by(1)
+      expect(page).to have_current_path(test_case_path(TestCase.first.id))
+    end
+  end
+
+  context "whit invalid attributes" do
+    subject do
+      click_on "Criar"
+    end
+
+    it "doesn't create the test case" do
+      expect{ subject }.to change(TestCase, :count).by(0)
+      expect(page).to have_selector("div.alert.alert-danger")
+    end
+  end
+end
