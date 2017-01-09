053bbb3ad7b0690b2fe209fc2e4326f5eabaf3c7
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-01-09 14:44:51 +0000

    Implement index action

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,20 @@
 class TeamsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @teams = find_teams
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+    private
+
+    # Return all teams or only the user teams.
+    def find_teams
+      return current_user.my_teams if params[:my_teams]
+      Team.active_teams
+    end
 end
