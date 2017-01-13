63788dc5a39dfc76cb18d9396fd5f8cb9f07e0ab
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-01-13 01:07:48 +0000

    Add new and create action to answers controller, and add new view with provisory layout

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,6 @@
 class AnswersController < ApplicationController
+  include ApplicationHelper
+
   before_action :authenticate_user!
   before_action :find_question
 
@@ -7,9 +9,31 @@ class AnswersController < ApplicationController
   end
 
   def create
+    @answer = @question.answers.build(answer_params)
+    @answer.user = current_user
+    @results = @question.test_all(plain_current_datetime, "pas", params[:content])
+    @answer.correct = correct?(@results)
+    if @answer.save
+      if @answer.correct?
+        flash.now[:success] = "Resposta correta!"
+      else
+        flash.now[:danger] = "Resposta incorreta!"
+      end
+    end
+
+    respond_to { |format| format.js }
   end
 
     private
+
+    def correct?(results)
+      results.each { |result| return false if result[:status] == :error }
+      true
+    end
+
+    def answer_params
+      params.require(:answer).permit(:content)
+    end
 
     def find_question
       @question = Question.find(params[:id])
