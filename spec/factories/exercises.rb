2ab9b74ac74e1d44dcc3f19cf8ce06040d2a85c6
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-01-13 19:06:39 +0000

    Add tests to exercise progress method

--- spec/factories/exercises.rb
+++ spec/factories/exercises.rb
@@ -3,5 +3,15 @@ FactoryGirl.define do
     title
     description "exercise description"
     user
+
+    factory :exercise_with_questions do
+      transient do
+        questions_count 0
+      end
+
+      after(:create) do |exercise, evaluator|
+        create_list(:question, evaluator.questions_count, exercise: exercise)
+      end
+    end
   end
 end
--- spec/models/exercise_spec.rb
+++ spec/models/exercise_spec.rb
@@ -32,4 +32,35 @@ RSpec.describe Exercise, type: :model do
       expect(relationship_type(Exercise, :teams)).to eq(:has_and_belongs_to_many)
     end
   end
+
+  describe '#user_progress' do
+    let(:user) { create(:user) }
+    let!(:exercise) { create(:exercise_with_questions, questions_count: 2) }
+
+    context "when none question is right answered" do
+      it "returns 0" do
+        expect(exercise.user_progress(user)).to eq(0)
+      end
+    end
+
+    context "when is incomplete, but with some questions answered correctly" do
+      before { create(:answer, :correct, question: Question.first, user: user) }
+
+      it "returns a value between 0 and 100" do
+        expect(exercise.user_progress(user)).to eq(50)
+      end
+    end
+
+    context "when all questions are right answered" do
+      before do
+        exercise.questions.each do |question|
+          question.answers << create(:answer, :correct, question: question, user: user)
+        end
+      end
+      
+      it "returns 100" do
+        expect(exercise.user_progress(user)).to eq(100)
+      end
+    end
+  end
 end
