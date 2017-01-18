72c7a2f90efea36ee21213ffd0ada9586b1b7058
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-01-18 20:15:46 +0000

    Change methods to add a answer to receive as parameter the user too

--- spec/support/helpers/answer_process_helper.rb
+++ spec/support/helpers/answer_process_helper.rb
@@ -1,5 +1,12 @@
 module Helpers
   module AnswerProcessHelper
+    # Note: when we say "create a right answer to question" it means that is
+    # added a test case to question (who should have none at this point) where
+    # the desired output is "Hello, world.". The factory answer receives as
+    # default content the pascal program to output  this sentence. In other
+    # hand, when we sar "create a wrong answer to question" whe create a answer
+    # wo is compiled succesfully, but to the question is added a test case who
+    # doesn't return "Hello, worl.", and so the answer is evaluated as wrong.
 
     def create_an_exercise_to_user(user)
       create(:exercise, user: user)
@@ -9,19 +16,22 @@ module Helpers
       create(:question, exercise: exercise)
     end
 
-    def create_right_answer_to_question(question)
+    def create_right_answer_to_question(question, user = nil)
       create(:test_case, :hello_world, question: question)
-      create(:answer, question: question, user: create(:user))
+      user ||= create(:user)
+      create(:answer, question: question, user: user)
+    end
+    def create_wrong_answer_to_question(question, user = nil)
+      
+      create(:test_case, question: question)
+      user ||= create(:user)
+      create(:answer, question: question, user: user)
     end
 
-    def create_wrong_answer_to_question(question)
+    def create_answer_who_not_compile_to_question(question, user = nil)
       create(:test_case, question: question)
-      create(:answer, question: question)
-    end
-
-    def create_answer_who_not_compile_to_question(question)
-      create(:test_case, question: question)
-      create(:answer, :invalid_content, question: question)
+      user ||= create(:user)
+      create(:answer, :invalid_content, question: question, user: user)
     end
   end
 end
