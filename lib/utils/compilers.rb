9a5d4f5b469f66d064fb6fa0f6bc76f217ded5b4
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-01-20 11:48:50 +0000

    Add the option to compile and run separately

--- lib/utils/compilers.rb
+++ lib/utils/compilers.rb
@@ -2,35 +2,41 @@ module Compilers
   PATH_TO_SAVE = "tmp/"
   COMPILER_OUTPUT_END_FILE_NAME = "_compiler_output"
 
-  # Main method to run a file. If compilations presents errors, show the
-  # compiler log; else run the file and returns its output.
-  def run(file_name, file_ext, source_code, input)
-    compile(file_name, file_ext, source_code)
-
-    if has_error?
-      compiler_output_content(file_name)
-    else
-      `./#{PATH_TO_SAVE}#{file_name} input`
-    end
+  # Compile the source code and run it, if compiled succesfully. Return the
+  # compiler output if not compiled succesfully, else the result of the
+  # source code exercution.
+  def compile_and_run(file_name, file_ext, source_code, input)
+    compiler_output = compile(file_name, file_ext, source_code)
+    return compiler_output if has_error?
+    run(file_name, file_ext, source_code, input)
   end
 
-  # Method to treat all the compilations processes.
-  def compile(file_name, file_ext, source_code)
-    file = file_full_path(file_name, file_ext)
+  # Run the compiled source code, or return the compiler log if you try to run
+  # a source code who doesn't compiled succesfully.
+  def run(file_name, file_ext, source_code, input)
+    return "Not possible run because compilation error:\n\n#{compiler_output_content(file_name)}" if has_error?
+    `./#{PATH_TO_SAVE}#{file_name} input`
+  end
+
+  # Compile the source code and return the compiler output.
+  def compile(filename, file_ext, source_code)
+    file = file_full_path(filename, file_ext)
     save_source_code(file, source_code)
 
-    # '-Fe' parameter says where save the compiler log.
     if file_ext == "pas"
-      `fpc #{file} -Fe#{compiler_output_full_path(file_name)}`
+      `fpc #{file} -Fe#{compiler_output_full_path(filename)}`
     else
       raise "The app doesn't support '.#{lang_ext}' files."
     end
+    compiler_output_content(filename)
   end
 
   # Check if compilation presents errors. 'exitstatus' gets the shell last
   # command status, so be sure to, if you need to check it, don't call it
-  # after execute other shell command.
+  # after execute other shell command. Also a error message is raised if you
+  # call this method without compiled the source code first.
   def has_error?
+    raise "Undefined 'exitstatus'. Have you compiled your source code first?" unless defined?($?.exitstatus)
     $?.exitstatus != 0
   end
 
--- spec/lib/utils/compilers_spec.rb
+++ spec/lib/utils/compilers_spec.rb
@@ -2,32 +2,101 @@ require 'rails_helper'
 require 'utils/compilers'
 
 include Compilers
+include ApplicationHelper
 
 describe Compilers do
   describe '#run' do
+    context "when source code is compiled -->" do
+      let!(:file_name) { plain_current_datetime }
+      before { compile(file_name, "pas", source_code) }
+
+      context "when source code is right" do
+        let(:source_code) { File.open("spec/support/files/hello_world.pas").read }
+        let!(:result) { run(file_name, "pas", source_code, "") }
+
+        it "doesn't return error" do
+          expect(has_error?).to be_falsey
+        end
+
+        it "returns the desired output" do
+          expect(result).to eq("Hello, world.\n")
+        end
+      end
+
+      context "when source code is wrong -->" do
+        let!(:file_name) { plain_current_datetime }
+        let(:source_code) { "That doesn't compile" }
+        let!(:result) { run(file_name, "pas", source_code, "") }
+
+        it "returns error" do
+          expect(has_error?).to be_truthy
+        end
+
+        it "returns the error as output" do
+          expect(result.scan(/(?i)error/).empty?).to be_falsey
+        end
+      end
+    end
+
+    # TODO: Need to find a way to realod the Compilers module, because
+    # '$?.exitstatus' from 'has_error?' is defined in the tests above, and
+    # here should be not defined to this test pass.
+    #
+    # context "when source code isn't compiled" do
+    #   subject {  run(plain_current_datetime, "pas", "some code", "") }
+
+    #   it "raises an error" do
+    #     expect { subject }.to raise_error(RuntimeError)
+    #   end
+    # end
+  end
+
+  describe '#compile_and_run' do
     context "when source code is right" do
       let(:source_code) { File.open("spec/support/files/hello_world.pas").read }
-      before { @result = run("test", "pas", source_code, "") }
+      let!(:result) { compile_and_run(plain_current_datetime, "pas", source_code, "") }
 
       it "doesn't return error" do
         expect(has_error?).to be_falsey
       end
 
       it "returns the desired output" do
-        expect(@result).to eq("Hello, world.\n")
+        expect(result).to eq("Hello, world.\n")
       end
     end
 
     context "when source code is wrong" do
-      let(:source_code) { "" }
-      before { @result = run("test", "pas", source_code, "") }
+      let(:source_code) { "That doesn't compile" }
+      let!(:result) { compile_and_run(plain_current_datetime, "pas", source_code, "") }
 
       it "returns error" do
         expect(has_error?).to be_truthy
       end
 
       it "returns the error as output" do
-        expect(@result.scan(/(?i)error/).empty?).to be_falsey
+        expect(result.scan(/(?i)error/).empty?).to be_falsey
+      end
+    end
+  end
+
+  describe '#compile' do
+    context "when source_code is wrong" do
+      let(:source_code) { "That doesn't compile" }
+      let!(:result) { compile(plain_current_datetime, "pas", source_code) }
+
+      it "returns error" do
+        expect(has_error?).to be_truthy
+        expect(result).to_not be_nil
+      end
+    end
+
+    context "when source code is right" do
+      let(:source_code) { File.open("spec/support/files/hello_world.pas").read }
+      let!(:result) { compile(plain_current_datetime, "pas", source_code) }
+
+      it "doesn't return error" do
+        expect(has_error?).to be_falsey
+        expect(result).to_not be_nil
       end
     end
   end
