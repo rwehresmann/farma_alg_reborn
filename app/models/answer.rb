05b75e332fbb902d074d1475e072126ca9310696
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-01-22 20:24:58 +0000

    Update answer model to call the job in after create callback

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -8,7 +8,7 @@ class Answer < ApplicationRecord
   before_create :set_correct
   # If compilator points error in source code, there is no need to
   # test with the question test cases.
-  after_create :save_test_cases_result, unless: :compilation_error?
+  after_create :save_test_cases_result
 
   validates_presence_of :content
   validates_inclusion_of :correct, in: [true, false]
@@ -16,8 +16,16 @@ class Answer < ApplicationRecord
   belongs_to :user
   belongs_to :question
   belongs_to :team
+  has_many :answer_connections, foreign_key: :answer_1_id
+  has_many :similarities, through: :answer_connections, source: :answer_2
   has_many :test_cases_results, class_name: "AnswerTestCaseResult"
   has_many :test_cases, through: :test_cases_results
+
+  def self.all_team_answers_to_question(team, question, options = {})
+    query = "team_id = ? AND question_id = ? "
+    return Answer.where(query.concat("AND id NOT IN (?)"), team, question, options[:except]) if options[:except]
+    Answer.where(query, team, question)
+  end
 
     private
 
@@ -32,6 +40,7 @@ class Answer < ApplicationRecord
       if has_error?
         self.compilation_error = true
         self.correct = false
+        @results = []
       else
         self.compilation_error = false
         # The source code is already compiled, so 'compile: false'.
@@ -43,6 +52,8 @@ class Answer < ApplicationRecord
     # Save the result accoring the result of each test case (@results is filled
     # from set_correct method).
     def save_test_cases_result
+      ComputeSimilarityJob.perform_later(self)
+
       @results.each do |result|
         AnswerTestCaseResult.create!(answer: self,
                                      test_case: result[:test_case],
