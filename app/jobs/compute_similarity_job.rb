e6481ab8fc1311c4e756b8835487f3b97f7ae151
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-01-22 20:23:21 +0000

    Implement ComputeSimilarityJob

--- app/jobs/compute_similarity_job.rb
+++ app/jobs/compute_similarity_job.rb
@@ -5,7 +5,11 @@ class ComputeSimilarityJob < ApplicationJob
 
   queue_as :default
 
-  def perform(answer)
-    
+  def perform(answer_1)
+    to_compare = Answer.all_team_answers_to_question(answer_1.team, answer_1.question, except: answer_1)
+    to_compare.each do |answer_2|
+      similarity = answers_similarity(answer_1, answer_2)
+      AnswerConnection.create!(answer_1: answer_1, answer_2: answer_2, similarity: similarity)
+    end
   end
 end
