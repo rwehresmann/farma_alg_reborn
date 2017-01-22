dece1e2fbda700c48a625fbbdc7ea1528bb2e422
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-01-22 00:05:24 +0000

    Remove unnecessary timestamps

--- db/migrate/20170121012119_create_teams_users.rb
+++ db/migrate/20170121012119_create_teams_users.rb
@@ -2,7 +2,6 @@ class CreateTeamsUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :teams_users do |t|
       t.references :team, :user
-      t.timestamps null: false
     end
   end
 end
