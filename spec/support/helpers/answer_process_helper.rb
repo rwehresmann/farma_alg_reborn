6645eadcaa6ac53e57ef95b9fa1aa94a8c6b5269
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-01-24 12:04:14 +0000

    Update helper whit the option of create an answers without trigger its custom callbakcs

--- spec/support/helpers/answer_process_helper.rb
+++ spec/support/helpers/answer_process_helper.rb
@@ -1,12 +1,16 @@
 module Helpers
   module AnswerProcessHelper
-    # Note: when we say "create a right answer to question" it means that is
-    # added a test case to question (who should have none at this point) where
-    # the desired output is "Hello, world.". The factory answer receives as
-    # default content the pascal program to output  this sentence. In other
-    # hand, when we sar "create a wrong answer to question" whe create a answer
-    # wo is compiled succesfully, but to the question is added a test case who
-    # doesn't return "Hello, worl.", and so the answer is evaluated as wrong.
+    # Note: when we say "create a right answer to question" and 'callbacks' is
+    # true it means that is added a test case to question (who should have none
+    # at this point) where the desired output is "Hello, world.". The factory
+    # answer receives as default content the pascal program to output this
+    # sentence. In other hand, when we sar "create a wrong answer to question"
+    # and 'callbacks' is true, whe create a answer wo is compiled succesfully,
+    # but to the question is added a test case who doesn't return
+    # "Hello, world.", and so the answer is evaluated as wrong. To all cases,
+    # when 'callbacks' is false, the custom callbacks from answer object aren't
+    # called and the attributes are setted automatically (correct,
+    # compilation_error).
 
     def create_an_exercise_to_user(user = nil)
       user ||= create(:user)
@@ -17,22 +21,37 @@ module Helpers
       create(:question, exercise: exercise)
     end
 
-    def create_right_answer_to_question(question, user = nil)
-      create(:test_case, :hello_world, question: question)
-      user ||= create(:user)
-      create(:answer, question: question, user: user)
-    end
-    def create_wrong_answer_to_question(question, user = nil)
-
-      create(:test_case, question: question)
-      user ||= create(:user)
-      create(:answer, question: question, user: user)
+    def create_right_answer_to_question(question, options = { user: nil, callbacks: false })
+      user = get_user(options[:user])
+      if options[:callbacks]
+        create(:test_case, :hello_world, question: question)
+        return create(:answer, :whit_custom_callbacks, question: question, user: user)
+      end
+      create(:answer, :correct, question: question, user: user)
     end
 
-    def create_answer_who_not_compile_to_question(question, user = nil)
-      create(:test_case, question: question)
-      user ||= create(:user)
-      create(:answer, :invalid_content, question: question, user: user)
+    def create_wrong_answer_to_question(question, options = { user: nil, callbacks: false })
+      user = get_user(options[:user])
+      if options[:callbacks]
+        create(:test_case, question: question)
+        return create(:answer, :whit_custom_callbacks, question: question, user: user)
+      end
+      create(:answer, question: question, user: user)
+    end
+
+    def create_answer_whit_compilation_error_to_question(question, options = { user: nil, callbacks: false })
+      user = get_user(options[:user])
+      if options[:callbacks]
+        create(:test_case, question: question)
+        return create(:answer, :invalid_content, :whit_custom_callbacks, question: question, user: user)
+      end
+      create(:answer, :invalid_content, :whit_custom_callbacks, question: question, user: user)
+    end
+
+    # Used as an auxiliary method in the other methods of this helper.
+    def get_user(user = nil)
+      return user if user
+      create(:user)
     end
   end
 end
