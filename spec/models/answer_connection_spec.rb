119b1aa2c467836062b29920399dd99104b81643
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-01-27 20:42:51 +0000

    Add a test to 'similarity' method

--- spec/models/answer_connection_spec.rb
+++ spec/models/answer_connection_spec.rb
@@ -20,4 +20,16 @@ RSpec.describe AnswerConnection, type: :model do
       expect(relationship_type(AnswerConnection, :answer_2)).to eq(:belongs_to)
     end
   end
+
+  describe ".similarity" do
+    let(:answer_1) { create(:answer) }
+    let(:answer_2) { create(:answer) }
+    let(:result) { AnswerConnection.similarity(answer_1, answer_2) }
+
+    before { AnswerConnection.create!(answer_1: answer_1, answer_2: answer_2, similarity: 100) }
+
+    it "returns the similarity of a connection" do
+      expect(result).to eq(100)
+    end
+  end
 end
