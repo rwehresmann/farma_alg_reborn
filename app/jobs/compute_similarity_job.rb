3dbd41aab9aab8aee3f5475c1f58032c61ac4144
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-01-28 13:32:28 +0000

    Update job to work with simetrical records

--- app/jobs/compute_similarity_job.rb
+++ app/jobs/compute_similarity_job.rb
@@ -5,11 +5,13 @@ class ComputeSimilarityJob < ApplicationJob
 
   queue_as :default
 
+  # Compute the similarity between all answers to que same question in the
+  # same team.
   def perform(answer_1)
     to_compare = Answer.all_team_answers_to_question(answer_1.team, answer_1.question, except: answer_1)
     to_compare.each do |answer_2|
       similarity = answers_similarity(answer_1, answer_2)
-      AnswerConnection.create!(answer_1: answer_1, answer_2: answer_2, similarity: similarity)
+      AnswerConnection.create_simetrical_record(answer_1, answer_2, similarity)
     end
   end
 end
