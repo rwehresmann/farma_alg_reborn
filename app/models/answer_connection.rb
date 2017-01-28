9ff12dea0c8ea8ee7f7d9287dab09e0384ad5fab
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-01-28 13:32:01 +0000

    Add 'create simetrical record' method and add its tests

--- app/models/answer_connection.rb
+++ app/models/answer_connection.rb
@@ -4,8 +4,15 @@ class AnswerConnection < ApplicationRecord
   belongs_to :answer_1, class_name: :Answer
   belongs_to :answer_2, class_name: :Answer
 
-  # Shortcut to get the resulted similarity between two answers
-  def self.similarity(answer_a, answer_b)
-    where(answer_1: answer_a, answer_2: answer_b).or(where(answer_1: answer_b, answer_2: answer_a)).pluck(:similarity).first
+  # Shortcut to get the resulted similarity between two answers.
+  def self.similarity(answer_1, answer_2)
+    where(answer_1: answer_1, answer_2: answer_2).or(where(answer_1: answer_2, answer_2: answer_1)).pluck(:similarity).first
+  end
+
+  # answer_1 similarity to answer_2 means the same similarity of answer_2
+  # to answer_1.
+  def self.create_simetrical_record(answer_1, answer_2, similarity)
+    AnswerConnection.create!(answer_1: answer_1, answer_2: answer_2, similarity: similarity)
+    AnswerConnection.create!(answer_1: answer_2, answer_2: answer_1, similarity: similarity)
   end
 end
--- spec/models/answer_connection_spec.rb
+++ spec/models/answer_connection_spec.rb
@@ -32,4 +32,18 @@ RSpec.describe AnswerConnection, type: :model do
       expect(result).to eq(100)
     end
   end
+
+  describe ".create_simetrical_record" do
+    let(:answer_1) { create(:answer) }
+    let(:answer_2) { create(:answer) }
+    let(:result_1) { AnswerConnection.similarity(answer_1, answer_2) }
+    let(:result_2) { AnswerConnection.similarity(answer_2, answer_1) }
+
+    before { AnswerConnection.create_simetrical_record(answer_1, answer_2, 100) }
+
+    it "creates a simetrical record" do
+      expect(result_1).to eq(100)
+      expect(result_2).to eq(100)
+    end
+  end
 end
