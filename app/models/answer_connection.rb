2ae1a464d30515852e63710d6873cf146f7f5fba
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-01-28 13:02:27 +0000

    Add commentaries to answer_connection class

--- app/models/answer_connection.rb
+++ app/models/answer_connection.rb
@@ -1,6 +1,11 @@
 class AnswerConnection < ApplicationRecord
   validates_presence_of :similarity
-  
+
   belongs_to :answer_1, class_name: :Answer
   belongs_to :answer_2, class_name: :Answer
+
+  # Shortcut to get the resulted similarity between two answers
+  def self.similarity(answer_a, answer_b)
+    where(answer_1: answer_a, answer_2: answer_b).or(where(answer_1: answer_b, answer_2: answer_a)).pluck(:similarity).first
+  end
 end
