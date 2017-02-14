adfb448e788a7844ea571226fa2869b307ec2055
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-02-14 13:23:21 +0000

    Create ranking query

--- app/models/user_score.rb
+++ app/models/user_score.rb
@@ -3,4 +3,24 @@ class UserScore < ApplicationRecord
 
   belongs_to :user
   belongs_to :team
+
+  scope :by_user, -> (user = nil) do
+    return unless user.present?
+    where(user: user)
+  end
+
+  scope :by_team, -> (team = nil) do
+    return unless team.present?
+    where(team: team)
+  end
+
+  scope :top, -> (x = nil) do
+    return unless x.present?
+    limit(x)
+  end
+
+  scope :rank, -> (options = {}) do
+    by_user(options[:user]).by_team(options[:team])
+    .top(options[:limit]).order(score: :desc)
+  end
 end
