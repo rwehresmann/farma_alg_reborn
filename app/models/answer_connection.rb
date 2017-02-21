d33210290eb7097729a63bed949c4fe4c395785c
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-02-21 22:42:46 +0000

    Minor changes

--- app/models/answer_connection.rb
+++ app/models/answer_connection.rb
@@ -5,14 +5,15 @@ class AnswerConnection < ApplicationRecord
   belongs_to :answer_2, class_name: :Answer
 
   # Shortcut to get the resulted similarity between two answers.
-  def self.similarity(answer_1, answer_2)
-    where(answer_1: answer_1, answer_2: answer_2).or(where(answer_1: answer_2, answer_2: answer_1)).pluck(:similarity).first
+  scope :similarity, -> (answer_1, answer_2)do
+    where(answer_1: answer_1, answer_2: answer_2)
+    .or(where(answer_1: answer_2, answer_2: answer_1)).pluck(:similarity).first
   end
 
   # answer_1 similarity to answer_2 means the same similarity of answer_2
   # to answer_1.
   def self.create_simetrical_record(answer_1, answer_2, similarity)
-    AnswerConnection.create!(answer_1: answer_1, answer_2: answer_2, similarity: similarity)
-    AnswerConnection.create!(answer_1: answer_2, answer_2: answer_1, similarity: similarity)
+    create!(answer_1: answer_1, answer_2: answer_2, similarity: similarity)
+    create!(answer_1: answer_2, answer_2: answer_1, similarity: similarity)
   end
 end
