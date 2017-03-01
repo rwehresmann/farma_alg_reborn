3cd19ea9398d1f6857062f78a0d8e5401506256d
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-03-01 14:17:43 +0000

    Remove 'now' call from flash in rescue_from

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   rescue_from CanCan::AccessDenied do |exception|
-    # 'now' is required because else the flash doesn't desapear when page is
-    # reloaded.
-    flash.now[:alert] = exception.message
+    flash[:warning] = exception.message
     respond_to do |format|
-      format.html { redirect_to request.referrer || root_url }
+      format.html { redirect_to request.referer || root_url }
       format.js   { render 'shared/unauthorized' }
     end
   end
