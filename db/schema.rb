73440ac3f8bcba73a8da48eb1945a9b6da4ff376
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-03-13 16:24:29 +0000

    Update migrations

--- db/schema.rb
+++ db/schema.rb
@@ -166,6 +166,7 @@ ActiveRecord::Schema.define(version: 20170228201615) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
+    t.string   "name",                                   null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -176,7 +177,6 @@ ActiveRecord::Schema.define(version: 20170228201615) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
-    t.string   "name"
     t.boolean  "teacher",                default: false, null: false
     t.boolean  "admin",                  default: false, null: false
     t.string   "anonymous_id"
