457341734cb7d435ad18355d45900ff28af9cc12
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-03-22 13:24:27 +0000

    Add diff view in answer modal

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,2 +1,11 @@
 module AnswersHelper
+  def correct_icon(answer)
+    return "fa fa-check" if answer.correct?
+    "fa fa-close"
+  end
+
+  def correct_box_style(answer)
+    return "box-success" if answer.correct?
+    "box-danger"
+  end
 end
