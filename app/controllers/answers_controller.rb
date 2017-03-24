90bd0b72e80d04680312f606473313aa794e7a8b
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2017-03-24 14:02:59 +0000

    Add @node_html_id in show action

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,5 @@
 class AnswersController < ApplicationController
+  include AnswerObjectToGraph
   include ApplicationHelper
 
   before_action :authenticate_user!
@@ -19,6 +20,7 @@ class AnswersController < ApplicationController
   end
 
   def show
+    @node_html_id = params[:node_html_id]
     respond_to { |format| format.js }
   end
 
@@ -59,36 +61,5 @@ class AnswersController < ApplicationController
 
     def find_question
       @question = Question.find(params[:id])
-    end
-
-    # Get answer data used in views.
-    def answer_data(answer)
-      answer.slice(:id, :content, :correct, :compilation_error,
-                               :compiler_output, :created_at).symbolize_keys
-    end
-
-    # Get user data used in views.
-    def user_data(user)
-      user.slice(:id, :name).symbolize_keys
-    end
-
-    # Get exercise data used in views.
-    def exercise_data(exercise)
-      exercise.slice(:id, :title).symbolize_keys
-    end
-
-    # Get question data used in views.
-    def question_data(question)
-      question.slice(:id, :title).symbolize_keys
-    end
-
-    # Create a hash with the necessary data to display in the graph.
-    def answer_object_to_graph(answer)
-      hash = {}
-      hash.merge!(answer_data(answer))
-      hash[:user] = user_data(answer.user)
-      hash[:question] = question_data(answer.question)
-      hash[:exercise] = exercise_data(answer.question.exercise)
-      hash
     end
 end
