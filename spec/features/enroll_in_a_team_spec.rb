b7c01892f11e3ba414e0f8bbcac010fc46d3b434
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-04-04 21:59:51 -0300

    Create test to 'enroll in a team' feature

---
+++ spec/features/enroll_in_a_team_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe "Enroll in a team", type: :feature, js: true do
+  let!(:team) { create(:team) }
+
+  before do
+    login_as create(:user)
+    visit teams_path
+  end
+
+  context "with a valid password" do
+    before do
+      fill_in "password", with: team.password
+      find('[name=commit]').click
+    end
+
+    it "redirects to team page" do
+      expect(page).to have_current_path(team_path(team))
+      expect(page).to have_selector("div.alert.alert-success")
+    end
+  end
+
+  context "with an invalid password" do
+    before do
+      fill_in "password", with: ""
+      find('[name=commit]').click
+    end
+
+    it "informs that the password is wrong" do
+      expect(page).to have_current_path(teams_path)
+      expect(page).to have_content("Senha incorreta!")
+    end
+  end
+end
