ee53c4df1240396d7bc9a20bf0112d00f504499f
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-04-06 08:34:50 -0300

    Update seeds with new data

--- db/seeds.rb
+++ db/seeds.rb
@@ -79,6 +79,7 @@ User.all.each do |user|
         correct = [true, false].sample
         answer = FactoryGirl.create(:answer, question: question, team: team,
                            user: user, correct: correct)
+        answer.comments.create!(user: team.owner, content: "This is a comment.")
         question.test_cases.each do |test_case|
           FactoryGirl.create(:answer_test_case_result, answer: answer, test_case: test_case,
                                   output: answer.compiler_output, correct: correct)
