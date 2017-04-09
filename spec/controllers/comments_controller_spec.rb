6216a4f6ae16f7b7b82b9041ecdbcc74375b4cdc
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-04-08 23:12:08 -0300

    Add tests to create and destroy action from CommentsController

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,6 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
+  describe "POST #create" do
+    let(:user) { create(:user, :teacher) }
+    let(:answer) { create(:answer) }
+
+    subject { post :create, xhr: true, params: { answer_id: answer, comment: { content: "content" } }  }
+
+    context "when logged-in" do
+      before do
+        sign_in user
+        subject
+      end
+
+      it { expect(response).to have_http_status(:ok) }
+      it { expect(response.content_type).to eq("text/javascript") }
+      it { expect(response).to render_template("comments/create") }
+    end
+
+    context "when logged-out" do
+      before { subject }
+
+      it { expect(response).to have_http_status(:unauthorized) }
+      it { expect(flash[:warning]).to_not be_nil }
+      it { expect(response).to render_template("shared/unauthorized") }
+    end
+  end
+
+  describe "PUT #update" do
+    let(:user) { create(:user, :teacher) }
+    let!(:comment) { create(:comment, user: user) }
+
+    subject { put :update, xhr: true, params: { id: comment, comment: { content: "content" } }  }
+
+    context "when logged-in" do
+      before do
+        sign_in user
+        subject
+      end
+
+      it { expect(response).to have_http_status(:ok) }
+      it { expect(response.content_type).to eq("text/javascript") }
+      it { expect(response).to render_template("comments/update") }
+    end
+
+    context "when logged-out" do
+      before { subject }
+
+      it { expect(response).to have_http_status(:unauthorized) }
+      it { expect(flash[:warning]).to_not be_nil }
+      it { expect(response).to render_template("shared/unauthorized") }
+    end
+  end
+
   describe "DELETE #destroy" do
     let(:user) { create(:user, :teacher) }
     let!(:comment) { create(:comment, user: user) }
