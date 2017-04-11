04b78af869f49210e84207952b6f73792e8c88eb
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-04-11 17:49:28 -0300

    Add test to the feature of 'add and remove exercises in a team'

---
+++ spec/features/add_exercise_to_a_team_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "Add exercise to a team", type: :feature, js: true do
+  let!(:user) { create(:user, :teacher) }
+  let!(:exercise) { create(:exercise, user: user) }
+  let(:team) { create(:team, owner: user) }
+
+  before do
+    login_as user
+    visit team_path(team)
+
+    click_on "Listas de exercício"
+    click_on "Adicionar exercício"
+    find("#add-exercise-#{exercise.id} a", text: "Adicionar").trigger("click")
+  end
+
+  it "replaces the add button and add the exercise to the table" do
+    expect(page).to have_selector("#add-exercise-#{exercise.id} span", text: "Adicionado")
+    expect(page).to have_selector("#team-exercise-#{exercise.id} td a", text: exercise.title)
+  end
+end
---
+++ spec/features/remove_exercise_from_a_team_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "Add exercise to a team", type: :feature, js: true do
+  let(:user) { create(:user, :teacher) }
+  let(:exercise) { create(:exercise, user: user) }
+  let!(:team) { create(:team, owner: user, exercises: [exercise]) }
+
+  before do
+    login_as user
+    visit team_path(team)
+
+    click_on "Listas de exercício"
+    click_on "Adicionar exercício"
+    find("#remove-exercise-#{exercise.id}").trigger("click")
+  end
+
+  it "replaces the add button and add the exercise to the table" do
+    expect(page).to have_selector("#add-exercise-#{exercise.id} a", text: "Adicionar")
+    expect(page).to_not have_selector("#team-exercise-#{exercise.id} td a", text: exercise.title)
+  end
+end
