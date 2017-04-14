29ca8cf6aa0aac63262e1e06bf4bd5ec6b2d9f0b
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-04-14 14:23:11 -0300

    Simplify CodeRunner and add support to multiple params

---
+++ lib/utils/code_runner.rb
@@ -0,0 +1,84 @@
+class CodeRunner
+  PATH_TO_SAVE = "tmp/"
+  COMPILER_OUTPUT_END_FILE_NAME = "_compiler_output"
+  COMPILED_LANGUAGES = ["pas"]
+
+  attr_accessor :file_name, :extension, :source_code
+
+  # @file_path = the source code file name (.ext) plus the path where should be saved.
+  # @compiler_log_path = the same that @file_path, but instead the source code,
+  #                      is the compiler log that is saved.
+  def initialize(args = {})
+    @file_name = args[:file_name]
+    @extension = args[:extension]
+    @file_path = "#{PATH_TO_SAVE}#{file_name}.#{extension}"
+    @compiler_log_path = "#{PATH_TO_SAVE}#{file_name}#{COMPILER_OUTPUT_END_FILE_NAME}.txt"
+    @source_code = args[:source_code]
+  end
+
+  # Main method to call.
+  def run(options = {})
+    if compiled_language?
+      compile unless options[:not_compile]
+      return compiler_output_content if has_compilation_error?
+    end
+
+    case extension
+      when "pas"
+        command = "./#{PATH_TO_SAVE}#{file_name}"
+        `#{to_exec(command, options[:params])}`
+      else
+        raise "CodeRunner doesn't support '.#{extension}' files."
+    end
+  end
+
+  def compile
+    save_source_code
+
+    case @extension
+      when "pas"
+        `fpc #{@file_path} -Fe#{@compiler_log_path}`
+      else
+        raise "CodeRunner doesn't support '.#{extension}' files."
+    end
+  end
+
+    private
+
+    # Join command to be executed with the informed params, if there are params.
+    def to_exec(command, params)
+      params.each { |param| command.concat(" #{param}") } if params
+      command
+    end
+
+    # If file with the compiler log is created, that points that the source code
+    # is already compiled.
+    def compiled?
+      File.file?(@compiler_log_path)
+    end
+
+    # If only check exitstatus, that can leave errors if the code runner
+    # compile a sequence of source codes, like in a test suite, for example. So
+    # we check first if the filed is compiled, and then the exitstatus which is
+    # indeed the exitstatus from the compilation of this source code (because
+    # is called right after the compile method, inside run method).
+    def has_compilation_error?
+      return $?.exitstatus != 0 if compiled?
+      raise "You need compile your source code first."
+    end
+
+    def save_source_code
+      File.open(@file_path, 'w') { |f| f.write(source_code) }
+    end
+
+    # Get the compiler log output as a string.
+    def compiler_output_content
+      File.open(@compiler_log_path).read
+    end
+
+    # Check if the extension represents an extension of a compiled language
+    # that CodeRunner is prepared to support.
+    def compiled_language?
+      COMPILED_LANGUAGES.include?(@extension)
+    end
+end
---
+++ spec/lib/utils/code_runner_spec.rb
@@ -0,0 +1,77 @@
+require 'rails_helper'
+require 'utils/code_runner'
+
+describe CodeRunner do
+  describe '#run' do
+    context "when try to run a pascal source code" do
+      context "with input" do
+        let(:file_name) { SecureRandom.hex }
+        let(:source_code) { IO.read("spec/support/files/params.pas") }
+        let(:code_runner) { CodeRunner.new(file_name: SecureRandom.hex,
+                                           extension: "pas", source_code: source_code) }
+
+        context "of one param" do
+          subject { code_runner.run(params: [1]) }
+          it { expect(subject).to match("There are two parameters required. You provided 1") }
+        end
+
+        context "of more than one param" do
+          subject { code_runner.run(params: [1, 2]) }
+
+          it { expect(subject).to match("1 + 2 = 3\n") }
+        end
+      end
+
+      context "when source code is right" do
+        let(:file_name) { SecureRandom.hex }
+        let(:source_code) { IO.read("spec/support/files/hello_world.pas") }
+        let!(:code_runner) { CodeRunner.new(file_name: file_name, extension: "pas",
+                                            source_code: source_code) }
+
+        it "returns the source code output" do
+          result = code_runner.run
+          expect(result).to eq("Hello, world.\n")
+        end
+      end
+
+      context "when not_compile: true" do
+        let(:source_code) { IO.read("spec/support/files/hello_world.pas") }
+
+        subject { code_runner.run(not_compile: true) }
+
+        context "when already compiled" do
+          let(:file_name) { SecureRandom.hex }
+          let!(:code_runner) { CodeRunner.new(file_name: file_name, extension: "pas",
+                                              source_code: source_code) }
+
+          before do
+            code_runner.send(:save_source_code)
+            code_runner.send(:compile)
+          end
+
+          it { expect(subject).to eq("Hello, world.\n") }
+        end
+
+        context "when isn't already compiled" do
+          let(:file_name) { SecureRandom.hex }
+          let!(:code_runner) { CodeRunner.new(file_name: file_name, extension: "pas",
+                                              source_code: source_code) }
+
+          it { expect { subject }.to raise_error(RuntimeError) }
+        end
+      end
+
+      context "when source code has compilation errors" do
+        let(:file_name) { SecureRandom.hex }
+        let(:source_code) { IO.read("spec/support/files/hello_world_compilation_error.pas") }
+        let!(:code_runner) { CodeRunner.new(file_name: file_name, extension: "pas",
+                                            source_code: source_code) }
+
+        it "returns the compiler log" do
+          result = code_runner.run
+          expect(result.scan(/(?i)error/).empty?).to be_falsey
+        end
+      end
+    end
+  end
+end
