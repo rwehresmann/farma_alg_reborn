7325a1edecca96a5bd334d64bd87f29817e9f48f
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-05-07 00:02:37 -0300

    Remove scopes from QuestionDepedency model

--- app/models/question_dependency.rb
+++ app/models/question_dependency.rb
@@ -7,24 +7,9 @@ class QuestionDependency < ApplicationRecord
   validates_inclusion_of :operator, in: DEPENDENCIES
   validate :belongs_to_same_exercise
 
-  scope :with_question_1, -> (question) do
-    return unless question.present?
-    where(question_1: question)
+  private
+
+  def belongs_to_same_exercise
+    errors.add("Cannot create dependencies between questions in different exercises.") if question_1.exercise != question_2.exercise
   end
-
-  scope :with_question_2, -> (question) do
-    return unless question.present?
-    where(question_2: question)
-  end
-
-  scope :by_operator, -> (operator) do
-    return unless operator.present?
-    where(operator: operator)
-  end
-
-    private
-
-    def belongs_to_same_exercise
-      errors.add("Cannot create dependencies between questions in different exercises.") if question_1.exercise != question_2.exercise
-    end
 end
