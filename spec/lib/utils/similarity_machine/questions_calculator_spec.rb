1b3911f68cb58d1320a933efc09db5cd4edb4ffc
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-05-12 22:30:43 -0300

    Refact calculators tests

--- spec/lib/utils/similarity_machine/questions_calculator_spec.rb
+++ spec/lib/utils/similarity_machine/questions_calculator_spec.rb
@@ -10,136 +10,189 @@ describe SimilarityMachine::QuestionsCalculator do
   describe '#questions_similarity' do
     context "when questions aren't informed" do
       subject {
-        calculator(*create_pair(:user)).calculate_similarity([])
+        described_class.new(
+          user_1: create(:user),
+          user_2: create(:user),
+          team: create(:team)
+        ).calculate_similarity([])
       }
 
-      it { expect { subject }.to raise_error(ArgumentError) }
+      it {
+        expect { subject }.to raise_error(ArgumentError)
+      }
     end
 
     context "when questions are informed" do
       context "when none answers of the users have its similarity calculated already" do
-        subject {
-          calculator(
-            *users_without_answers_similarity_calculated
-          ).calculate_similarity([@question])
-        }
+        it "returns 0% of similarity" do
+          users = create_list(:user, 3)
+          teams = create_pair(:team)
+          questions = create_list(:question, 3)
 
-        it { is_expected.to eq 0 }
+          answers = answers_hash(
+            users: users,
+            teams: teams,
+            questions: questions
+          )
+
+          create_connections_who_must_be_avoided(answers)
+
+          similarity = described_class.new(
+            user_1: users[0],
+            user_2: users[1],
+            team: teams[0]
+          ).calculate_similarity(questions.first(2))
+
+          expect(similarity).to eq 0
+        end
       end
 
       context "when some users answers have its similarity calculated already" do
         context "when they're completely similar" do
-          subject {
-            calculator(
-              *users_with_completely_similar_answers
-            ).calculate_similarity([@question])
-          }
+          it "returns 100% of similarity" do
+            users = create_list(:user, 3)
+            teams = create_pair(:team)
+            questions = create_list(:question, 3)
 
-          it { is_expected.to eq 100 }
+            answers = answers_hash(
+              users: users,
+              teams: teams,
+              questions: questions
+            )
+
+            create(
+              :answer_connection,
+              answer_1: answers[:user_0][:team_0][:question_0],
+              answer_2: answers[:user_1][:team_0][:question_0],
+              similarity: 100
+            )
+
+            create(
+              :answer_connection,
+              answer_1: answers[:user_0][:team_0][:question_1],
+              answer_2: answers[:user_1][:team_0][:question_1],
+              similarity: 100
+            )
+
+            create_connections_who_must_be_avoided(answers)
+
+            similarity = described_class.new(
+              user_1: users[0],
+              user_2: users[1],
+              team: teams[0]
+            ).calculate_similarity(questions.first(2))
+
+            expect(similarity).to eq 100
+          end
         end
 
         context "when they're similar in some level" do
-          subject {
-            calculator(
-              *users_with_similar_answers
-            ).calculate_similarity([@question])
-          }
+          it "returns something >= 0% and <= 100% of similarity" do
+            users = create_list(:user, 3)
+            teams = create_pair(:team)
+            questions = create_list(:question, 3)
 
-          it { is_expected.to eq 75 }
+            answers = answers_hash(
+              users: users,
+              teams: teams,
+              questions: questions
+            )
+
+            create(
+              :answer_connection,
+              answer_1: answers[:user_0][:team_0][:question_0],
+              answer_2: answers[:user_1][:team_0][:question_0],
+              similarity: 100
+            )
+
+            create(
+              :answer_connection,
+              answer_1: answers[:user_0][:team_0][:question_1],
+              answer_2: answers[:user_1][:team_0][:question_1],
+              similarity: 50
+            )
+
+            create_connections_who_must_be_avoided(answers)
+
+            similarity = described_class.new(
+              user_1: users[0],
+              user_2: users[1],
+              team: teams[0]
+            ).calculate_similarity(questions.first(2))
+
+            expect(similarity).to eq 75
+          end
         end
 
         context "when they're completely different" do
-          subject {
-            calculator(
-              *users_with_completely_different_answers
-            ).calculate_similarity([@question])
-          }
+           it "returns 0% of similarity" do
+             users = create_list(:user, 3)
+             teams = create_pair(:team)
+             questions = create_list(:question, 3)
 
-          it { is_expected.to eq 0 }
+             answers = answers_hash(
+               users: users,
+               teams: teams,
+               questions: questions
+             )
+
+             create(
+               :answer_connection,
+               answer_1: answers[:user_0][:team_0][:question_0],
+               answer_2: answers[:user_1][:team_0][:question_0],
+               similarity: 0
+             )
+
+             create(
+               :answer_connection,
+               answer_1: answers[:user_0][:team_0][:question_1],
+               answer_2: answers[:user_1][:team_0][:question_1],
+               similarity: 0
+             )
+
+             create_connections_who_must_be_avoided(answers)
+
+             similarity = described_class.new(
+               user_1: users[0],
+               user_2: users[1],
+               team: teams[0]
+             ).calculate_similarity(questions.first(2))
+
+             expect(similarity).to eq 0
+           end
         end
       end
     end
   end
 
-  def calculator(user_1, user_2)
-    described_class.new(user_1: user_1, user_2: user_2, team: @team)
-  end
-
-  def users_without_common_questions(team)
-    team = create(:team)
-    users = create_pair(:user)
-    users.each { |user| create(:answer, team: team, user: user) }
-    users
-  end
-
-  def users_without_answers_similarity_calculated
-    users = create_pair(:user)
-    users.each { |user|
-      create(:answer, team: @team, user: user, question: @question)
-    }
-    users
-  end
-
-  def users_with_completely_similar_answers
-    users = create_pair(:user)
-    answers = answers_to_create_connections(
-      users: users, team: @team, question: @question
-    )
+  # This method shares common data setup from the tests, creating answers
+  # connections who must not be considered because doesn't match with the
+  # arguments informed do AnswersCalculator and its calculate_similarity method
+  # (this helps to test unexpected behavior in the tests).
+  # The tests area configured with:
+  # - 3 users, where users[2] must be avoided;
+  # - 2 teams, where teams[1] must be avoided;
+  # - 3 questions, where questions[2] must be avoided.
+  def create_connections_who_must_be_avoided(answers)
     create(
       :answer_connection,
-      answer_1: answers[:user_1][0],
-      answer_2: answers[:user_2][0],
-      similarity: 100
+      answer_1: answers[:user_0][:team_1][:question_0],
+      answer_2: answers[:user_2][:team_1][:question_0],
+      similarity: 94
     )
 
-    users
-  end
-
-  def users_with_completely_different_answers
-    users = create_pair(:user)
-    answers = answers_to_create_connections(
-      users: users, team: @team, question: @question
-    )
     create(
       :answer_connection,
-      answer_1: answers[:user_1][0],
-      answer_2: answers[:user_2][0],
-      similarity: 0
+      answer_1: answers[:user_0][:team_1][:question_0],
+      answer_2: answers[:user_1][:team_1][:question_0],
+      similarity: 86
     )
 
-    users
-  end
-
-  def users_with_similar_answers
-    users = create_pair(:user)
-    answers = answers_to_create_connections(
-      users: users, team: @team, question: @question
-    )
     create(
       :answer_connection,
-      answer_1: answers[:user_1][0],
-      answer_2: answers[:user_2][0],
-      similarity: 50
+      answer_1: answers[:user_0][:team_0][:question_2],
+      answer_2: answers[:user_1][:team_0][:question_2],
+      similarity: 32
     )
-    create(
-      :answer_connection,
-      answer_1: answers[:user_1][0],
-      answer_2: answers[:user_2][1],
-      similarity: 100
-    )
-
-    users
-  end
-
-  def answers_to_create_connections(users:, team:, question:)
-    answers = {}
-    answers[:user_1] = create_pair(
-      :answer, team: team, user: users[0], question: question
-    )
-    answers[:user_2] = create_pair(
-      :answer, team: team, user: users[1], question: question
-    )
-
-    answers
   end
 end
--- spec/lib/utils/similarity_machine/users_calculator_spec.rb
+++ spec/lib/utils/similarity_machine/users_calculator_spec.rb
@@ -6,14 +6,19 @@ describe SimilarityMachine::UsersCalculator do
 
   describe '#calculate_similarity' do
     context "when users haven't common questions answered" do
-      subject do
-        users = users_without_common_questions
-        described_class.new(
-          user_1: users[0], user_2: users[1], team: @team
-        ).calculate_similarity
-      end
+      it "returns nil" do
+        team = create(:team)
+        users = create_pair(:user)
+        users.each { |user| create(:answer, team: team, user: user) }
 
-      it { is_expected.to be_nil }
+        similarity = described_class.new(
+          user_1: users[0],
+          user_2: users[1],
+          team: team
+        ).calculate_similarity
+
+        expect(similarity).to be_nil
+      end
     end
 
     context "when users have common questions answered" do
@@ -24,51 +29,48 @@ describe SimilarityMachine::UsersCalculator do
         ).calculate_similarity
       end
 
-      it { is_expected.to eq 75 }
+      it "returns the similarity based in these questions answers connections" do
+        users = create_list(:user, 3)
+        teams = create_pair(:team)
+        questions = create_pair(:question)
+
+        answers = answers_hash(users: users, teams: teams, questions: questions)
+
+        create(
+          :answer_connection,
+          answer_1: answers[:user_0][:team_0][:question_0],
+          answer_2: answers[:user_1][:team_0][:question_0],
+          similarity: 50
+        )
+        create(
+          :answer_connection,
+          answer_1: answers[:user_0][:team_0][:question_1],
+          answer_2: answers[:user_1][:team_0][:question_1],
+          similarity: 100
+        )
+
+        # Must be avoided >>
+        create(
+          :answer_connection,
+          answer_1: answers[:user_0][:team_0][:question_1],
+          answer_2: answers[:user_2][:team_0][:question_1],
+          similarity: 72
+        )
+        create(
+          :answer_connection,
+          answer_1: answers[:user_0][:team_1][:question_1],
+          answer_2: answers[:user_1][:team_1][:question_1],
+          similarity: 87
+        )
+
+        similarity = described_class.new(
+          user_1: users[0],
+          user_2: users[1],
+          team: teams[0]
+        ).calculate_similarity
+
+        expect(similarity).to eq 75
+      end
     end
-  end
-
-  def users_without_common_questions
-    users = create_pair(:user)
-    users.each { |user| create(:answer, team: @team, user: user) }
-    users
-  end
-
-  def users_with_similar_answers
-    users = create_pair(:user)
-    answers = answers_to_create_connections(
-      users: users, team: @team
-    )
-
-    create(
-      :answer_connection,
-      answer_1: answers[:user_1][:question_1],
-      answer_2: answers[:user_2][:question_1],
-      similarity: 50
-    )
-    create(
-      :answer_connection,
-      answer_1: answers[:user_1][:question_2],
-      answer_2: answers[:user_2][:question_2],
-      similarity: 100
-    )
-
-    users
-  end
-
-  def answers_to_create_connections(users:, team:)
-    questions = create_pair(:question)
-    answers = {}
-
-    answers[:user_1] = {
-      question_1: create(:answer, team: team, user: users[0], question: questions[0]),
-      question_2: create(:answer, team: team, user: users[0], question: questions[1]),
-    }
-    answers[:user_2] = {
-      question_1: create(:answer, team: team, user: users[1], question: questions[0]),
-      question_2: create(:answer, team: team, user: users[1], question: questions[1]),
-    }
-
-    answers
   end
 end
