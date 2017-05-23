83b2f290ec66c10eccd97e0b25cd02411a212d18
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-05-22 23:56:09 -0300

    Added a return when there is <= 1 objects in array informed in compare_and_shift_each structure + Implemented tests to SimilarityMachine::Utils

--- lib/utils/similarity_machine/utils.rb
+++ lib/utils/similarity_machine/utils.rb
@@ -2,7 +2,6 @@ module SimilarityMachine
   module Utils
     def common_questions_answered(users:, team:)
       groups = []
-
       compare_and_shift_each(users, groups) do |user_1, user_2|
         users_questions = [user_1, user_2].map { |user|
           QuestionQuery.new.questions_answered_by_user(user, team: team).to_a
@@ -16,6 +15,7 @@ module SimilarityMachine
     def compare_and_shift_each(array, groups)
       array_copy = array.dup
       array.count.times do
+        return if array_copy.length <= 1
         object_1 = array_copy.shift
         array_copy.each { |object_2| yield(object_1, object_2) }
       end
---
+++ spec/lib/utils/similarity_machine/utils_spec.rb
@@ -0,0 +1,79 @@
+require 'rails_helper'
+require 'utils/similarity_machine/utils'
+
+describe SimilarityMachine::Utils do
+  let(:object) { Object.new.extend(described_class) }
+
+  describe '#compare_and_shift_each' do
+    subject do
+      trace = []
+      object.compare_and_shift_each(array, trace) { |object_1, object_2|
+        trace << [object_1, object_2]
+      }
+
+      trace
+    end
+
+    context "when array size is 0" do
+      let(:array) { [] }
+
+      it "doesn't executes the loop" do
+        is_expected.to be_empty
+      end
+    end
+
+    context "when array size is 1" do
+      let(:array) { [1] }
+
+      it "doesn't executes the loop" do
+        is_expected.to be_empty
+      end
+    end
+
+    context "when array size is bigger than 1" do
+      let(:array) { [1,2,3] }
+
+      it "executes the loop matching each element with all, removing it from the array after" do
+        is_expected.to eq [[1,2], [1,3], [2,3]]
+      end
+    end
+  end
+
+  describe '#common_questions_answered' do
+    context "when there are common questions answered" do
+      it "returns these questions" do
+        users = create_pair(:user)
+        questions = create_pair(:question)
+        teams = create_pair(:team, users: users)
+
+        users.each { |user|
+          create(:answer, question: questions[0], user: user, team: teams[0])
+        }
+
+        # These answers must be ignored >>
+        create(:answer, question: questions[1], user: users[0], team: teams[0])
+        users.each { |user|
+          create(:answer, question: questions[1], user: user, team: teams[1])
+        }
+
+        result = object.common_questions_answered(team: teams[0], users: users)
+        expect(result).to eq [questions[0]]
+      end
+    end
+
+    context "when there aren't common questions answered" do
+      it "returns an empty array" do
+        users = create_pair(:user)
+        questions = create_pair(:question)
+        team = create(:team, users: users)
+
+        # These answers must be ignored >>
+        create(:answer, question: questions[0], user: users[0], team: team)
+        create(:answer, question: questions[1], user: users[1], team: team)
+
+        result = object.common_questions_answered(team: team, users: users)
+        expect(result).to be_empty
+      end
+    end
+  end
+end
