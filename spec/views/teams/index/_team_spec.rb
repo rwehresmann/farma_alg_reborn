9bc54a4df8099f978c6ab291285bba2a41d19dcf
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-05-24 12:35:08 -0300

    Fixed tests from _team partial

--- spec/views/teams/index/_team_spec.rb
+++ spec/views/teams/index/_team_spec.rb
@@ -1,36 +1,30 @@
 require 'rails_helper'
 
-describe 'teams/_team.html.erb' do
+describe 'teams/index/_team.html.erb' do
+  before do
+    allow(view).to receive_messages(current_user: user)
+    allow(controller).to receive_messages(current_ability: Ability.new(user))
+    render 'teams/index/team', team: team
+  end
+
   context "when the current user is enrolled" do
     let!(:user) { create(:user) }
     let!(:team) { create(:team, users: [user]) }
-
-    before do
-      allow(view).to receive_messages(current_user: user)
-      allow(controller).to receive_messages(current_ability: Ability.new(user))
-      render partial: 'teams/team', locals: { team: team }
-    end
 
     it "displays team information" do
       expect(rendered).to have_content(team.name)
       expect(rendered).to have_content(team.owner.name)
     end
 
-    it { expect(rendered).to have_link("Ver", href: team_path(user)) }
+    it { expect(rendered).to have_link("Ver", href: rankings_team_path(team)) }
     it { expect(rendered).to have_link("Cancelar matrícula", href: unenroll_team_path(team)) }
     it { expect(rendered).to_not have_link("Deletar") }
     it { expect(rendered).to_not have_link("Editar") }
   end
 
-  context "when the current user is unenrolled" do
+  context "when the current user isn't enrolled" do
     let!(:user) { create(:user) }
     let!(:team) { create(:team) }
-
-    before do
-      allow(view).to receive_messages(current_user: user)
-      allow(controller).to receive_messages(current_ability: Ability.new(user))
-      render partial: 'teams/team', locals: { team: team }
-    end
 
     it "displays team information" do
       expect(rendered).to have_content(team.name)
@@ -46,12 +40,6 @@ describe 'teams/_team.html.erb' do
   context "when user is a tacher" do
     let!(:user) { create(:user, :teacher) }
     let!(:team) { create(:team, owner: user) }
-
-    before do
-      allow(view).to receive_messages(current_user: user)
-      allow(controller).to receive_messages(current_ability: Ability.new(user))
-      render partial: 'teams/team', locals: { team: team }
-    end
 
     it { expect(rendered).to have_link("Excluir") }
     it { expect(rendered).to have_link("Editar") }
