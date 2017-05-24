6b302b8bd75618f3d5c33bcc2960fd1bd73d278a
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-05-24 15:36:24 -0300

    Fixed _nav_tabs partial tests

---
+++ spec/views/teams/_nav_tabs_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe "teams/_nav_tabs.html.erb", type: :view do
+  let(:partial) { 'teams/nav_tabs' }
+  let(:user) { create(:user, :teacher) }
+
+  before do
+    allow(view).to receive_messages(current_user: user)
+    allow(controller).to receive_messages(current_ability: Ability.new(user))
+  end
+
+  context "when user is the owner of the team" do
+    it "renders all tabs options" do
+      render partial, team: create(:team, owner: user)
+
+      expect(rendered).to have_link("Rankings")
+      expect(rendered).to have_link("Listas")
+      expect(rendered).to have_link("Alunos matriculados")
+      expect(rendered).to have_link("Grafo de manipulação")
+    end
+  end
+
+  context "when team doesn't belongs to the user" do
+    it "renders all tabs options" do
+      render partial, team: create(:team, users: [user])
+
+      expect(rendered).to have_link("Rankings")
+      expect(rendered).to have_link("Listas")
+      expect(rendered).to have_link("Alunos matriculados")
+      expect(rendered).to_not have_link("Grafo de manipulação")
+    end
+  end
+end
