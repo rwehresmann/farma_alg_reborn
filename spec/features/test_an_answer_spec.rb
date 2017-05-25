c80d49933913a65e109b9f9502b3dc2bd68ca4b2
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-05-25 09:28:44 -0300

    Fixed 'test answer' feature test

--- spec/features/test_an_answer_spec.rb
+++ spec/features/test_an_answer_spec.rb
@@ -15,37 +15,51 @@ describe "Test an answer", type: :feature, js: true do
   end
 
   context "when no anwer is given" do
-    before { submit_answer }
+    it "displays an alert div" do
+      user = create(:user, :teacher)
+      exercise = create(:exercise, user: user)
+      question = create(:question, exercise: exercise)
 
-    it { expect(page).to have_selector("div.alert.alert-danger") }
+      login_as user
+      visit question_path(question)
+      click_on "Testar solução"
+      click_on "Submeter resposta"
+
+      expect(page).to have_selector("div.alert.alert-danger")
+    end
   end
 
   context "when the answer is right" do
-    let(:source_code) { IO.read("spec/support/files/hello_world.pas") }
+    it "informs tat the answer is right" do
+      user = create(:user, :teacher)
+      exercise = create(:exercise, user: user)
+      question = create(:question, exercise: exercise)
+      create(:test_case, output: "Hello, world.\n", question: question)
 
-    before do
-      fill_in_editor_field(source_code)
-      submit_answer
-    end
+      login_as user
+      visit question_path(question)
+      click_on "Testar solução"
+      fill_in_editor_field(IO.read("spec/support/files/hello_world.pas"))
+      click_on "Submeter resposta"
 
-    before { create(:test_case, question: question, output: "Hello, world.\n") }
-
-    it "informs that the answer is right and show test cases results" do
       expect(page).to have_content("Resposta correta")
       expect(page).to have_selector('#test-results div.alert.alert-success')
     end
   end
 
   context "when answer is wrong" do
-    let(:source_code) { "This is a wrong answer" }
+    it "informs tat the answer is right" do
+      user = create(:user, :teacher)
+      exercise = create(:exercise, user: user)
+      question = create(:question, exercise: exercise)
+      create(:test_case, output: "Hello, world.\n", question: question)
 
-    before do
-      fill_in_editor_field(source_code)
-      submit_answer
-      page.save_screenshot("lalala.png")
-    end
+      login_as user
+      visit question_path(question)
+      click_on "Testar solução"
+      fill_in_editor_field("This is a wrong answer")
+      click_on "Submeter resposta"
 
-    it "informs that the answer is wrong and show test cases results" do
       expect(page).to have_content("Resposta incorreta")
       expect(page).to have_selector('#test-results div.alert.alert-danger')
     end
