6f351dcabec631371d4f8ecb273cd7abfac513eb
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-06-05 09:35:49 -0300

    Configured logger in development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,11 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Show logs in console.
+  logger = ActiveSupport::Logger.new(STDOUT)
+  logger.formatter = config.log_formatter
+  config.logger = ActiveSupport::TaggedLogging.new(logger)
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
