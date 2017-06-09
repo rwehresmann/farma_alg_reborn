2fa0b9da1a749c0a70dae33db67eda4a5152a2b7
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-06-09 12:30:52 -0300

    Initial design of home view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,11 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def home
+    if current_user.teacher?
+      @teams_answers = TeacherTeamsLastAnswersQuery.new(teacher: current_user).call
+      # @recommendations =
+    end
+
   end
 
 end
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,6 @@
 module DashboardHelper
+  def answer_status_label(answer)
+    return raw "<span class='badge bg-green'>Correta</span>" if answer.correct
+    raw "<span class='badge bg-red'>Incorreta</span>"
+  end
 end
