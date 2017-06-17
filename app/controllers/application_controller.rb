f359c0e9be5e7f5193c818cc5d43c7035be579ab
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-06-16 23:38:25 -0300

    Prettified log in and sign up views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,6 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :teacher])
     end
 end
