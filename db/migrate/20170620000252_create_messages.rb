4ce2955acf2d6fd11fdbea8056a8086434182f9e
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-06-21 23:22:23 -0300

    Changed Message content from string to text

--- db/migrate/20170620000252_create_messages.rb
+++ db/migrate/20170620000252_create_messages.rb
@@ -2,8 +2,9 @@ class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
       t.string :title, null: false
-      t.string :content, null: false
+      t.text :content, null: false
       t.references :sender, references: :User
+      t.references :receiver, references: :User
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -97,11 +97,13 @@ ActiveRecord::Schema.define(version: 20170620000252) do
   end
 
   create_table "messages", force: :cascade do |t|
-    t.string   "title",      null: false
-    t.string   "content",    null: false
+    t.string   "title",       null: false
+    t.text     "content",     null: false
     t.integer  "sender_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "receiver_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["receiver_id"], name: "index_messages_on_receiver_id", using: :btree
     t.index ["sender_id"], name: "index_messages_on_sender_id", using: :btree
   end
 
