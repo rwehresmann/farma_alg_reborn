b353d82f3a0b27766ab9843f218540bfdb2a4e76
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-06-24 17:59:09 -0300

    Modified condition

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,14 +2,11 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def home
-    if current_user.teacher?
-      @teams_answers = TeacherTeamsLastAnswersQuery.new(teacher: current_user).call
-      @user_answers = UserLastAnswersQuery.new(current_user).call
-      @user_last_messages = current_user.messages_received
-        .order(created_at: :desc)
-        .limit(5)
-    end
-
+    @teams_answers = TeacherTeamsLastAnswersQuery.new(teacher: current_user).call if current_user.teacher?
+    @user_answers = UserLastAnswersQuery.new(current_user).call
+    @user_last_messages = current_user.messages_received
+      .order(created_at: :desc)
+      .limit(5)
   end
 
 end
