f667f57c136f1f75fde84de73a98daf0423c1e9c
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-07-10 09:24:53 -0300

    Added constants to identify the log type

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,2 +1,14 @@
 class Log < ApplicationRecord
+  MSG_SEND    = "msg_send"
+  RECOM_SEND  = "recom_send"
+  ANSW_SHOW   = "answ_show"
+  EDGE_SHOW   = "edge_show"
+  EDGE_REJECT = "edge_reject"
+  ANSW_SEARCH = "answ_search"
+  ANS_NEW_TRY = "answ_new_try"
+  ANS_SEND    = "answ_send"
+
+  belongs_to :user
+
+  validates_presence_of :type
 end
