fce166f6c5aaebd8ef9b57cac73d4d9bdf4b250b
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-07-31 09:41:46 -0300

    Removed comments to be able to run  Whenever

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,8 +3,8 @@
 bundle check || bundle install
 
 # Uncomment these lines when whant to use Whenever gem
-# bundle exec whenever --update-crontab --set environment='development'
-# service cron start
+bundle exec whenever --update-crontab # --set environment='development'
+service cron start
 
 bundle exec bin/delayed_job start
 
