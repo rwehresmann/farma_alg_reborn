ee1d8431ecb81a67ad69441e4a94e21e5edb87b2
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-08-01 17:29:30 -0300

    Updated schedule to call tasks each minute (just for test pourposes)

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,14 +2,10 @@ ENV.each { |k, v| env(k, v) }
 
 set :output, "log/whenever.log"
 
-every 3.hours do
+every 1.minute do #3.hours do
   rake "recommendate_answers"
 end
 
-every :day, at: "12pm" do
+every 1.minute do #:day, at: "12pm" do
   rake "update_initial_ranking_day"
-end
-
-every 1.minute do
-  runner "puts 'CALLED!'"
 end
