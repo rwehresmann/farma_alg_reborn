82c0462a59553684c0ec5c0743ad809fc2c0fc61
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-08-04 21:56:43 -0300

    Added 'edit profile' feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,9 +11,10 @@ class ApplicationController < ActionController::Base
     end
   end
 
-    protected
+  protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :teacher])
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :teacher])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
