8c3788767f908dc4f6bfc84daeb29144931cc3fc
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-08-24 00:04:03 -0300

    Fixed answer connections query test

--- spec/factories/answer_connections.rb
+++ spec/factories/answer_connections.rb
@@ -2,14 +2,14 @@ FactoryGirl.define do
   factory :answer_connection do
     association :answer_1, factory: :answer
     association :answer_2, factory: :answer
-    similarity 5.0
+    similarity 100
 
     trait :high_similarity do
-      similarity 9.0
+      similarity 90
     end
 
     trait :low_similarity do
-      similarity 1.0
+      similarity 10
     end
   end
 end
