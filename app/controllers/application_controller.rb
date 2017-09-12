e0ba9022856dd230def85f1631b8005384372e78
Alex Kutzke <alexkutzke@gmail.com>
2017-09-12 19:55:15 -0300

    Relative_url_root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,16 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :teacher])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to new_user_session_path	
+    end
+  end
 end
